60385b01c886efdd0ffa59ffa8800354ce995582
Amanda <amanda.alexandre@outlook.com>
2022-04-27 10:03:51 -0300

    style: finished top 10 carousel styling

--- js/carousel.js
+++ js/carousel.js
@@ -16,3 +16,22 @@ let slider = tns({
         }
       }
   });
+
+  let top10 = tns({
+    container: '.top-10',
+    items: 4,
+    slideBy: 'page',
+    mouseDrag: 'mouseDrag',
+    autoplay: false,
+    controlsContainer: '#controls-top-10',
+    prevButton: '#previous-top-10',
+    nextButton: '#next-top-10',
+    responsive: {
+        800: {
+          items: 3
+        },
+        1000: {
+          items: 6
+        }
+      }
+  });

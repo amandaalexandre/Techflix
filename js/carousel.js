8b310d4553aa5b3da2d7212baa6749b65c396d8b
Amanda <amanda.alexandre@outlook.com>
2022-04-27 11:29:32 -0300

    feat: inserting numbers in top 10 carousel

--- js/carousel.js
+++ js/carousel.js
@@ -19,13 +19,54 @@ let slider = tns({
 
   let top10 = tns({
     container: '.top-10',
+    items: 3,
+    slideBy: 'page',
+    mouseDrag: 'mouseDrag',
+    autoplay: false,
+    autoWidth: true,
+    loop: false,
+    rewind: true,
+    controlsContainer: '#controls-top-10',
+    prevButton: '#previous-top-10',
+    nextButton: '#next-top-10',
+    responsive: {
+        800: {
+          items: 3
+        },
+        1000: {
+          items: 6
+        }
+      }
+  });
+
+let movies = tns({
+    container: '.movies',
     items: 4,
     slideBy: 'page',
     mouseDrag: 'mouseDrag',
     autoplay: false,
-    controlsContainer: '#controls-top-10',
-    prevButton: '#previous-top-10',
-    nextButton: '#next-top-10',
+    controlsContainer: '#movies-top-10',
+    prevButton: '#previous-movies',
+    nextButton: '#next-movies',
+    responsive: {
+        800: {
+          items: 3
+        },
+        1000: {
+          items: 6
+        }
+      }
+  });
+
+  let series = tns({
+    container: '.series',
+    items: 4,
+    slideBy: 'page',
+    mouseDrag: 'mouseDrag',
+    autoplay: false,
+    controlsContainer: '#series-top-10',
+    prevButton: '#previous-series',
+    nextButton: '#next-series',
     responsive: {
         800: {
           items: 3
